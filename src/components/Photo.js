530699d12a7584170bee03d011e15de0fd676d56
msnabiel <89602212+msnabiel@users.noreply.github.com>
2024-06-30 10:06:15 +0530

    Update Photo.js

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -15,7 +15,19 @@ function App() {
       {data ? (
         <div className="content">
           <h1>🌌 {data.title}</h1>
-          <img src={data.url} alt={data.title} />
+          {data.media_type === 'image' ? (
+            <img src={data.url} alt={data.title} />
+          ) : data.media_type === 'video' ? (
+            <div className="video-container">
+              <iframe
+                src={data.url}
+                title={data.title}
+                frameBorder="0"
+                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+              ></iframe>
+            </div>
+          ) : null}
           <p>{data.explanation}</p>
           <footer>
             <p>🛰️ Data provided by NASA's APOD API</p>
